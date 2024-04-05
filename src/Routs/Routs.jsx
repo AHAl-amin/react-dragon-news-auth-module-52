2020949b3f0e86a505d0685868bf402d4bbca0b1
Alamin <mdalaminhossain573190@gmail.com>
2024-04-05 12:49:24 +0600

    added login part

--- src/Routs/Routs.jsx
+++ src/Routs/Routs.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import Root from "../Layouts/Root";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
             }
         ]
     }
--- src/Shared/Header/LeftSideNav/LeftSideNav.jsx
+++ src/Shared/Header/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories ,setCategories] = useState([]);
+    useEffect(() =>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then (data => setCategories(data))
+    },[])
     return (
-        <div className="border">
-            <h2 className="2xl">Left side</h2>
+        <div className="border space-y-5">
+            <h2 className="2xl">All categories</h2>
+            {
+                categories.map(category => <Link className="block mx-5 my-2 font-semibold text-gray-400" key={category.id }
+                to={`/category/ ${category.id}`}>{category.name}</Link>)
+            }
         </div>
     );
 };
